4d90d7f444a0b7e9e2bd57a972a264492981707d
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-03-01 14:11:49 +0530

    Added Singleton, StorageAccesible and fixed Dummy Master Copy Contract

--- contracts/DummyMasterCopy.sol
+++ contracts/DummyMasterCopy.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.7.0 <0.9.0;
 
+import "./common/Singleton.sol";
+import "./common/StorageAccessible.sol";
+
 /**
  * @title DummyMasterCopy - Dummy master copy contract to test the proxy factory.
  * @author Anoy Roy Chowdhury - <anoyroyc3545@gmail.com>
  */
-contract DummyMasterCopy{
+contract DummyMasterCopy is Singleton, StorageAccessible{
     string public name;
 
     constructor() {
