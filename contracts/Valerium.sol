0cc96f848db9132b3517fee6b264cbe846d7c345
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-03-03 13:26:15 +0530

    Added Main Implementation of Valerium Wallet

---
+++ contracts/Valerium.sol
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./common/Singleton.sol";
+import "./common/StorageAccessible.sol";
+import "./base/Executor.sol";
+import "./base/ProofManager.sol";
+import "./handler/TokenCallbackHandler.sol";
+import "./external/ERC2771Context.sol";
+
+/**
+ * @title Valerium - A Smart Contract Wallet powered by ZK-SNARKs with support for Cross-Chain Transactions
+ * @dev Most important concepts :
+ *    - TxVerifier: Address of the Noir based ZK-SNARK verifier contract that will be used to verify proofs and execute transactions on the Valerium Wallet
+ *    - RecoveryVerifier: Address of the Noir based ZK-SNARK verifier contract that will be used to verify proofs and execute recovery transactions on the Valerium Wallet
+ *    - Gas Tank: The gas tank is a smart contract that deducts gas fees from the user's Valerium Wallet.
+ * @author Anoy Roy Chowdhury - <anoyroyc3545@gmail.com>
+ */
+
+contract Valerium is
+    Singleton,
+    StorageAccessible,
+    Executor,
+    ProofManager,
+    TokenCallbackHandler,
+    ERC2771Context
+{
+    string public constant VERSION = "1.0.0";
+
+    // The domain of the Valerium Wallet
+    bytes32 public DOMAIN = "0x";
+
+    // The address of the Noir based ZK-SNARK verifier contracts
+    address public TxVerifier;
+    address public RecoveryVerifier;
+
+    // The hash used as a public inputs for verifiers
+    bytes32 private TxHash;
+    bytes32 private RecoveryHash;
+
+    // The address of the gas tank contract
+    address public GasTank;
+
+    // The public storage of the Valerium Wallet
+    bytes public PublicStorage;
+
+    // The nonce for the Valerium Wallet
+    uint256 private nonce;
+
+    /**
+     * @notice Initializes the Valerium Wallet with the domain and the verifier contracts
+     * @dev This method can only be called once.
+     *      If the proxy was created without setting up, anyone can call setup and claim the proxy
+     * @param _domain The keccak256 hash of the domain of the Valerium Wallet
+     * @param _txVerifier The address of the Noir based ZK-SNARK verifier contract for verifying transactions
+     * @param _recoveryVerifier The address of the Noir based ZK-SNARK verifier contract for executing recovery transactions
+     * @param _forwarder The address of the trusted forwarder (ERC2771)
+     * @param _gasTank The address of the gas tank contract
+     */
+    function setupValerium (
+        bytes32 _domain,
+        address _txVerifier,
+        address _recoveryVerifier,
+        address _forwarder,
+        address _gasTank,
+        bytes32 _txHash,
+        bytes32 _recoveryHash,
+        bytes memory _publicStorage
+    ) external {
+        // Checking if the contract is already initialized
+        require(DOMAIN == "0x", "Valerium: already initialized");
+        require(TxVerifier == address(0), "Valerium: already initialized");
+        require(RecoveryVerifier == address(0), "Valerium: already initialized");
+        require(GasTank == address(0), "Valerium: already initialized");
+
+        // Setting up the trusted forwarder
+        setupTrustedForwarder(_forwarder);
+        DOMAIN = _domain;
+        TxVerifier = _txVerifier;
+        RecoveryVerifier = _recoveryVerifier;
+        GasTank = _gasTank;
+        TxHash = _txHash;
+        RecoveryHash = _recoveryHash;
+        PublicStorage = _publicStorage;
+    }
+
+    /**
+     * @notice Executes a transaction with provided parameters
+     * @param _proof The proof input
+     * @param to The address of the receiver
+     * @param value The amount of Ether to send
+     * @param data The data payload
+     * @return success boolean flag indicating if the call succeeded
+     */
+    function executeTx (bytes calldata _proof, address to, uint256 value, bytes calldata data) public payable returns(bool success) {
+        // Verifying the proof
+        require(verify(_proof, _useNonce(), TxHash, TxVerifier), "Valerium: invalid proof");
+        // Executing the transaction
+        success = execute(to, value, data, gasleft());
+    }
+    
+    /**
+     * @notice Executes a batch of transactions with provided parameters
+     * @param _proof The proof input
+     * @param to Array of destination addresses
+     * @param value Array of Ether values
+     * @param data Array of data payloads
+     */
+    function executeBatchTx (bytes calldata _proof, address[] calldata to, uint256[] calldata value, bytes[] calldata data) public payable {
+        // Verifying the proof
+        require(verify(_proof, _useNonce(), TxHash, TxVerifier), "Valerium: invalid proof");
+        // Executing the batch transactions
+        batchExecute(to, value, data);
+    }
+
+    /**
+     * @notice Executes a recovery transaction to change the transaction hash, transaction verifier and public storage
+     * @param _proof The proof input
+     * @param _newTxHash The new transaction hash
+     * @param _newTxVerifier The address of the new transaction verifier
+     * @param _publicStorage The new public storage
+     */
+    function executeRecovery(bytes calldata _proof, bytes32 _newTxHash, address _newTxVerifier, bytes calldata _publicStorage) public payable {
+        // Verifying the proof
+        require(verify(_proof, _useNonce(), RecoveryHash, RecoveryVerifier), "Valerium: invalid proof");
+        // Updating the Tx Hash, Tx Verifier and Public Storage
+        TxHash = _newTxHash;
+        TxVerifier = _newTxVerifier;
+        PublicStorage = _publicStorage;
+    }   
+
+    /**
+     * @notice Executes a recovery transaction to change the recovery hash, recovery verifier and public storage
+     * @param _proof The proof input
+     * @param _newRecoveryHash The new recovery hash
+     * @param _newRecoveryVerifier The address of the new recovery verifier
+     * @param _publicStorage The new public storage
+     */
+    function ChangeRecovery(bytes calldata _proof, bytes32 _newRecoveryHash, address _newRecoveryVerifier, bytes calldata _publicStorage) public payable {
+        // Verifying the proof
+        require(verify(_proof, _useNonce(), RecoveryHash, RecoveryVerifier), "Valerium: invalid proof");
+        // Updating the Recovery Hash, Recovery Verifier and Public Storage
+        RecoveryHash = _newRecoveryHash;
+        RecoveryVerifier = _newRecoveryVerifier;
+        PublicStorage = _publicStorage;
+    }
+
+    /**
+     * @notice Returns the nonce of the Valerium Wallet
+     * @return The nonce
+     */
+    function getNonce() public view returns (uint256) {
+        return nonce;
+    }
+
+    /**
+     * @notice Returns the nonce of the Valerium Wallet but increments it for the next use
+     * @return The nonce
+     */
+    function _useNonce() internal returns (uint256) {
+        unchecked {
+            return nonce++;
+        }
+    }
+}
---
+++ contracts/external/ERC2771Context.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.1) (metatx/ERC2771Context.sol)
+
+pragma solidity ^0.8.20;
+
+import {Context} from "./Context.sol";
+
+/**
+ * @dev Context variant with ERC-2771 support. This version of the context is used to support the Valerium Wallet.
+ * @author Anoy Roy Chowdhury - <anoyroyc3545@gmail.com>
+ *
+ * WARNING: Avoid using this pattern in contracts that rely in a specific calldata length as they'll
+ * be affected by any forwarder whose `msg.data` is suffixed with the `from` address according to the ERC-2771
+ * specification adding the address size in bytes (20) to the calldata size. An example of an unexpected
+ * behavior could be an unintended fallback (or another function) invocation while trying to invoke the `receive`
+ * function only accessible if `msg.data.length == 0`.
+ *
+ * WARNING: The usage of `delegatecall` in this contract is dangerous and may result in context corruption.
+ * Any forwarded request to this contract triggering a `delegatecall` to itself will result in an invalid {_msgSender}
+ * recovery.
+ */
+abstract contract ERC2771Context is Context {
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    address private _trustedForwarder;
+
+    /**
+     * @notice Sets the trusted forwarder for the context, could be called only once.
+     * @param forwarder The forwarder to be trusted
+     */
+    function setupTrustedForwarder(address forwarder) internal {
+        // Checking if the forwarder is already set
+        require(_trustedForwarder == address(0), "ERC2771Context: forwarder already set");
+        // Checking if the forwarder address is not zero
+        require(forwarder != address(0), "ERC2771Context: invalid trusted forwarder");
+        _trustedForwarder = forwarder;
+    }
+   
+    /**
+     * @dev Returns the address of the trusted forwarder.
+     */
+    function trustedForwarder() public view virtual returns (address) {
+        return _trustedForwarder;
+    }
+
+    /**
+     * @dev Indicates whether any particular address is the trusted forwarder.
+     */
+    function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
+        return forwarder == trustedForwarder();
+    }
+
+    /**
+     * @dev Modifier to check if the caller is the trusted forwarder.
+     */
+    modifier onlyTrustedForwarder() {
+        require(isTrustedForwarder(msg.sender), "ERC2771Context: caller is not the trusted forwarder");
+        _;
+        
+    }
+
+    /**
+     * @dev Override for `msg.sender`. Defaults to the original `msg.sender` whenever
+     * a call is not performed by the trusted forwarder or the calldata length is less than
+     * 20 bytes (an address length).
+     */
+    function _msgSender() internal view virtual override returns (address) {
+        uint256 calldataLength = msg.data.length;
+        uint256 contextSuffixLength = _contextSuffixLength();
+        if (isTrustedForwarder(msg.sender) && calldataLength >= contextSuffixLength) {
+            return address(bytes20(msg.data[calldataLength - contextSuffixLength:]));
+        } else {
+            return super._msgSender();
+        }
+    }
+
+    /**
+     * @dev Override for `msg.data`. Defaults to the original `msg.data` whenever
+     * a call is not performed by the trusted forwarder or the calldata length is less than
+     * 20 bytes (an address length).
+     */
+    function _msgData() internal view virtual override returns (bytes calldata) {
+        uint256 calldataLength = msg.data.length;
+        uint256 contextSuffixLength = _contextSuffixLength();
+        if (isTrustedForwarder(msg.sender) && calldataLength >= contextSuffixLength) {
+            return msg.data[:calldataLength - contextSuffixLength];
+        } else {
+            return super._msgData();
+        }
+    }
+
+    /**
+     * @dev ERC-2771 specifies the context as being a single address (20 bytes).
+     */
+    function _contextSuffixLength() internal view virtual override returns (uint256) {
+        return 20;
+    }
+}
